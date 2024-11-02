7e1344b20b64fd64fa4bdbcd5e752024d9f36742
Alex Straton <alexandrastraton1@gmail.com>
2024-11-02 13:16:20 +0000

    feat: display all adventures on the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div></div>;
+import { fetchAdventures } from "./lib/api_call";
+
+export default async function Home() {
+  const getAllAdventures = await fetchAdventures();
+
+  console.log(getAllAdventures);
+  return (
+    <div>
+      <h1>Adventures</h1>
+      <ul>
+        {getAllAdventures.map((adventure, index) => (
+          <li key={index}>
+            <h2>{adventure.name}</h2>
+            {adventure.image && (
+              <img
+                src={adventure.image}
+                alt={adventure.name}
+                width={200}
+                height={200}
+              />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
