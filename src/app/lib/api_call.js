62180e89afee7485193b2ed972b0e760c589d96f
Alex Straton <alexandrastraton1@gmail.com>
2024-11-02 14:52:16 +0000

    fix: account for missing image url and name in adventures fetch data call

--- src/app/lib/api_call.js
+++ src/app/lib/api_call.js
@@ -3,9 +3,25 @@ import axios from "axios";
 export async function fetchAdventures() {
   try {
     const res = await axios.get("https://jjzl6.wiremockapi.cloud/adventures");
+
     const adventures = res.data;
 
-    return adventures;
+    // const adventures = res.data.filter(
+    //   (adventure) => adventure.image && adventure.image !== "https:undefined"
+
+    const isValidUrl = (url) => {
+      try {
+        new URL(url);
+        return true;
+      } catch {
+        return false;
+      }
+    };
+    const adventuresArrayClean = adventures.filter(
+      (item) => item.name && item.image && isValidUrl(item.image)
+    );
+
+    return adventuresArrayClean;
   } catch (error) {
     console.error("An error has occured. Try again:", error);
     return {
